68b64d88a8ed8d3e96bd0f3473d78b6ec3b9729f
Michael Berend <michaelberend@hotmail.com>
2018-06-04 11:24:04 +0200

    Initial Commit

---
+++ PlannerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlannerApp
+//
+//  Created by Michael Berend on 04/06/2018.
+//  Copyright © 2018 Michael Berend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

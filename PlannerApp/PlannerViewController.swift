125c0e4c22145347b1017d5a4607272761d21223
Michael Berend <michaelberend@hotmail.com>
2018-06-07 15:54:41 +0200

    First protype

---
+++ PlannerApp/PlannerViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PlannerViewController.swift
+//  PlannerApp
+//
+//  Created by Michael Berend on 07/06/2018.
+//  Copyright © 2018 Michael Berend. All rights reserved.
+//
+
+import UIKit
+
+class PlannerViewController: UIViewController {
+
+    @IBOutlet weak var AddActivityButtonTapped: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func AddActivityButtonTapped(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Choose adding style", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { action in print("User selected camera")
+        })
+        let photoLibrabyAction = UIAlertAction(title: "Photo library", style: .default, handler: { action in
+            print("User selected Photo Library action")
+        })
+        alertController.addAction(cancelAction)
+        alertController.addAction(cameraAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
